af0bb49e6f562e279540bedebd6b1cc3dac20d9f
Valvixx <valvixx04@gmail.com>
2023-10-17 14:35:12 +0300

    Order Model Creating

---
+++ AutoService/Models/OrderModel.cs
@@ -0,0 +1,12 @@
+﻿using AutoServiceAPI.Models;
+namespace AutoService.Models
+{
+    public class Order
+    {
+        public Car OrderCar { get; set; }
+        public Customer User { get; set; }
+        public string Date { get; set;}
+        public string Description { get; set;}
+        public string Status { get; set;}
+    }
+}
---
+++ AutoService/Repository/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using AutoService.Models;
+
+namespace AutoService.Repository
+{
+    public class OrderRepository
+    {
+        private List<Order> orders = new List<Order>()
+        {
+            new Order
+            {
+                OrderCar = 
+            }
+        };
+
+    }
+}
